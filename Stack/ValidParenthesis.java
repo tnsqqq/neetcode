8c549c5f6543562d4142096d1ace561051e81809
tnsqqq <tnsqqqbaliyan@gmail.com>
2025-02-26 16:40:39 +0530

    32

---
+++ Stack/ValidParenthesis.java
@@ -0,0 +1,53 @@
+// import java.util.*;
+
+// public class ValidParenthesis {
+
+//   public static void main(String[] args) {
+//     String s = "()";
+//     System.out.println(isValid(s));
+//   }
+
+//   static boolean isValid(String s) {
+//     Stack<Character> stack = new Stack<Character>();
+//     for (char c : s.toCharArray()) {
+//       if (c == '(') {
+//         stack.push(')');
+//       } else if (c == '{') {
+//         stack.push('}');
+//       } else if (c == '[') {
+//         stack.push(']');
+//       } else if (stack.isEmpty() || stack.pop() != c) {
+//         return false;
+//       }
+//     }
+//     return stack.isEmpty();
+//   }}
+
+import java.util.*;
+
+public class ValidParenthesis {
+
+  public static void main(String[] args) {
+    String s = "()";
+    System.out.println(isValid(s));
+  }
+
+  static boolean isValid(String s) {
+
+    Stack<Character> stack = new Stack<>();
+
+    for (char c : s.toCharArray()) {
+      if (c == '(' || c == '{' || c == '[') {
+        stack.push(c);
+      } else {
+        if (stack.isEmpty() ||
+            (c == ')' && stack.pop() != '(') ||
+            (c == '}' && stack.pop() != '{') ||
+            (c == ']' && stack.pop() != '[')) {
+          return false;
+        }
+      }
+    }
+    return stack.isEmpty();
+  }
+}
